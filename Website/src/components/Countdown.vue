4b55280ec28855ef9c8422801f3473c301470e98
Anand911 <anandajith911@gmail.com>
2023-10-19 18:43:33 +0530

    Countdown Initial Commit

---
+++ Website/src/components/Countdown.vue
@@ -0,0 +1,33 @@
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet"></link>
+<!-- best viewed in Chrome -->
+<style>
+
+</style>
+<template>
+    <div class="nixie-container"></div>
+</template>
+<script>
+    export default{
+        props:[],
+        data(){
+            return{
+                eventDate:null,
+            }
+        
+        },
+        mounted()
+        {
+            this.init();
+        },
+        methods:
+        {
+            init()
+            {
+
+            }
+            //Other methods
+        }
+    }
+</script>
+
