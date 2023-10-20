b22f030331bef0eff525ad9df8c1b75a64ae88d0
Anand911 <djakashannad@gmail.com>
2023-10-20 22:18:19 +0530

    solved package confict

--- Website/src/App.vue
+++ Website/src/App.vue
@@ -1,27 +1,6 @@
-
-<script>
-import Countdown from './components/Countdown.vue';
-import FlipCountdown from './components/FlipCountdown.vue';
-import Main from './components/Main.vue'
-import Section1 from './components/Section1.vue';
-import Modal from './components/modal.vue';
-export default {
-  components: {
-    Main,
-    Section1
-},
-  data() {
-    return {
-      showModal: true
-    };
-  },
-  methods: {
-    closeModal() {
-      this.showModal = false;
-      console.log(this.showModal);
-    }
-  }
-};
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home/Home.vue';
 </script>
 <style>
 @import './assets/css/index.css';
