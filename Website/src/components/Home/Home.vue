b96d8bb770049c0c452965a7f4c17157924470e7
Anand911 <anandajith911@gmail.com>
2023-10-27 17:46:30 +0530

    FIrebase init, Countdown stage

--- Website/src/components/Home/Home.vue
+++ Website/src/components/Home/Home.vue
@@ -1,10 +1,12 @@
 <template>
+    <CountDown :show="data" @close="data=false"/>
     <MainClone/>
      <Section1/>
      <Section2/>     
 </template>
 
 <script>
+import CountDown from '../CountDown.vue';
 import MainClone from './MainClone.vue';
 import Section1 from './Section1.vue';
 import Section2 from './Section2.vue';
@@ -13,11 +15,17 @@ import TextMorph from './TextMorph.vue';
 
 
 export default {
+    data(){
+        return{
+            data:true,
+        }
+    },
     components: {
     MainClone,
     Section1,
     Section2,
     TextMorph,
+    CountDown
 },
     beforeRouteLeave(to, from, next) {
         // Reset your component's state here
--- Website/src/main.js
+++ Website/src/main.js
@@ -5,25 +5,46 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "./components/Home/Home.vue";
 import Games from "./components/Games/Content.vue";
 import Test from "./components/Test.vue";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDdVa3kY-11_jpsT_Fw6wBXPq4BBm7M_0g",
+  authDomain: "verite-45774.firebaseapp.com",
+  projectId: "verite-45774",
+  storageBucket: "verite-45774.appspot.com",
+  messagingSenderId: "340822673051",
+  appId: "1:340822673051:web:254f4d49dca48aef7ce83e",
+  measurementId: "G-K8G599E418"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const routes = [
     {
-        path: '/Verite/',
+        path: '/',
         name: 'Home',
         component: Home
     },
     {
-        path: '/Verite/games',
+        path: '/games',
         name: 'Games',
         component: Games
     },
     {
-        path:'/Verite/test',
+        path:'/test',
         name:'Test',
         component:Test
     }
 ]
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory('/'),
     routes
 })
 createApp(App).use(router).mount('#app')
